43a54ecb84596c6741ec48e387770a247fa771e1
munkh125 <munkh125@gmail.com>
2024-04-26 16:03:26 +0800

    finished rainbow grayscale

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const container = document.querySelector(".container");
 const controller = document.querySelector(".controller");
-const createBtn = document.querySelector("#createBtn");
 const numSquareInput = document.querySelector("#numSquareInput");
 const grid = document.querySelector(".grid");
+const blackBtn = document.querySelector("#blackBtn");
+const rainbowBtn = document.querySelector("#rainbowBtn");
+const grayBtn = document.querySelector("#grayBtn");
+
+let colorMode = "black";
 
 function resetGrid(){
     grid.innerHTML = "";
@@ -13,10 +17,10 @@ function createSquare(gridSize){
         let square = document.createElement("div");
         square.classList.add("square");
         let sideSize = 100/gridSize + "%"
-        square.setAttribute("style", "box-shadow: 0px 0px 0px 1px rgb(100, 100, 100) inset;")
+        //add border for square
+        // square.setAttribute("style", "box-shadow: 0px 0px 0px 1px rgb(100, 100, 100) inset;")
         square.style.width = sideSize
         square.style.height = sideSize
-        // square.style.border = "0.1px solid black"
         grid.appendChild(square);
     }
 }
@@ -31,12 +35,42 @@ function generateGrid(){
 }
 
 function highlight(e){
-    e.target.style.backgroundColor = "black";
-    // e.target.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    switch(colorMode){
+        case "black":
+            e.target.style.backgroundColor = "black";
+            break;
+        case "rainbow":
+            e.target.style.backgroundColor = `hsl(` + Math.floor(Math.random()* 360) + `, 100%, 50%)`;
+            break;
+        case "gray":
+            if(this.style.backgroundColor.match(/rgba/)){
+                let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
+                if (currentOpacity <= 0.8){
+                    this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+                    this.classList.add('gray');
+                }
+            }else if(this.classList == 'gray' && this.style.backgroundColor == 'rgb(0, 0, 0,'){
+                return
+            }else{
+                this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+            }
+            break;
+    }
 }
 
-createBtn.addEventListener("click", () => {
+blackBtn.addEventListener("click", () => {
+    colorMode = "black";
     generateGrid();
 });
+
+rainbowBtn.addEventListener("click", () => {
+    colorMode = "rainbow";
+    generateGrid();
+});
+
+grayBtn.addEventListener("click", () => {
+    colorMode = "gray";
+    generateGrid();
+})
 
 generateGrid();
